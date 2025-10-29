1fe6519d6e01b10c8b8e6840806480817ebd8d7d
marcus-ochoalujan <134079732+marcus-ochoalujan@users.noreply.github.com>
2025-10-29 00:19:23 -0500

    Add alien.py example for Chapter 6 dictionaries

---
+++ part_1_basics/chapter_06_dictionaries/alien.py
@@ -0,0 +1,9 @@
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+# The dicionary aline_0 stores the alien's color and point vlaue.
+# The last two lines access and display that information, as show here:
+# A dictionary in Python is a collection of key-value pairs.
+# Each key is connected to a value, and you can use a key to access the value associated with that key.
